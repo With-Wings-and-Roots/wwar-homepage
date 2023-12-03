36d6648c0c9017eb3182062510a4071e63131bc9
Joel <wwwebjoyal@gmail.com>
2023-12-03 14:59:31 +0545

    Add Newsletter

--- app/components/footer/newsLetter.jsx
+++ app/components/footer/newsLetter.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const NewsLetter = () => {
+  const language = useSelector((state) => state.entities.language.language);
   return (
     <div>
       <form
@@ -12,13 +14,17 @@ const NewsLetter = () => {
         target="_blank"
         noValidate
       >
-        <div id="mc_embed_signup_scroll">
-          <label htmlFor="mce-EMAIL">Subscribe</label>
+        <div id="mc_embed_signup_scroll" className="flex flex-wrap md:w-1/2">
+          <label htmlFor="mce-EMAIL" className="w-full text-lg pb-2 font-light">
+            {language === "de"
+              ? "Anmeldung zum Newsletter "
+              : "Subscribe to our newsletter to stay up to date: "}
+          </label>
           <input
             type="email"
             defaultValue=""
             name="EMAIL"
-            className="email input-field"
+            className="email input-field flex-grow pr-2 pl-4 tracking-wider text-xl font-light"
             id="mce-EMAIL"
             placeholder="email address"
             required
@@ -40,11 +46,17 @@ const NewsLetter = () => {
               value="Subscribe"
               name="subscribe"
               id="mc-embedded-subscribe"
-              className="button"
+              className="button px-3 tracking-normal uppercase text-xl bg-wwr_gray_storm h-12"
             />
           </div>
         </div>
       </form>
+      <div className="pt-10 text-wwr_gray_storm flex flex-wrap gap-2">
+        <div>Copyright 2023 With Wings & Roots. All rights reserved.</div>
+        <a href="/">
+          {language === "de" ? "Impressum" : "Terms and Conditions"}
+        </a>
+      </div>
     </div>
   );
 };
