83dc7cf6ba1f0a6068a66a14aeaaa7fefcaf37bc
volkram <volkram@volkramweber.de>
2023-12-30 16:59:31 +0100

    nextjs config fix

--- next.config.js
+++ next.config.js
@@ -8,6 +8,8 @@ module.exports = {
     remotePatterns: [
       {
         hostname: 'cms.withwingsandroots.org',
+      },
+      {
         hostname: 'wwar2022.backslashseven.com',
       },
     ],
