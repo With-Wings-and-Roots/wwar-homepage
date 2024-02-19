1748825b4852854865edaef79f86383893a8d886
volkram <volkram@volkramweber.de>
2024-02-19 22:45:19 +0100

    Revert "Collaborators Popup"
    This reverts commit 49a9d3a75e8319ed40d05553dbbaf2ea7a8ebfa6.

--- components/page/button.jsx
+++ components/page/button.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Button = ({ name, color, textDark=false }) => {
+const Button = ({ name, color }) => {
   return (
     <div
       className={`${
         color === 'turquoise' ? 'bg-wwr_turquoise' : 'bg-wwr_yellow_orange'
-      } ${textDark? 'text-wwr_rich_black':'text-wwr_white'} w-max py-2 px-4 text-base md:text-lg font-light`}
+      } w-max text-wwr_white py-2 px-4 text-base md:text-lg font-light`}
     >
       {name}
     </div>
