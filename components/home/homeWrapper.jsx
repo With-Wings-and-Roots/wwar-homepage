6f9c94ba90dfe6a5353b265e52c9e4af167aa035
volkram <volkram@volkramweber.de>
2024-02-19 22:45:21 +0100

    Revert "Update home"
    This reverts commit a0441512ccaf525cca7b86edc4d8c57155b3f8df.

--- components/home/homeWrapper.jsx
+++ components/home/homeWrapper.jsx
@@ -4,10 +4,10 @@ import TimelinesPageWrapper from '@/components/timelines/timelinesPageWrapper';
 
 const HomeWrapper=({lang, timeLineEventsDe, timeLineEventsEn, allMedia, searchParams, baseLink})=>{
   return(
-    <div className={`w-screen`}>
+    <>
       <HomeTitle />
       <TimelinesPageWrapper lang={lang} timeLineEventsDe={timeLineEventsDe} timeLineEventsEn={timeLineEventsEn} allMedia={allMedia} baseLink={baseLink} searchParams={searchParams } skip={true}/>
-    </div>
+    </>
   )
 }
 
--- components/templates/DefaultTemplate.jsx
+++ components/templates/DefaultTemplate.jsx
@@ -5,35 +5,31 @@ import HomeWrapper from '@/components/home/homeWrapper';
 
 const DefaultTemplate = ({ data, lang, timeLineEventsEn, timeLineEventsDe, allMedia, baseLink, searchParams }) => {
   return (
-    <>
-      <HomeWrapper lang={lang} timeLineEventsDe={timeLineEventsDe} timeLineEventsEn={timeLineEventsEn}
-                   allMedia={allMedia} baseLink={baseLink} searchParams={searchParams} />
-      {/*<div*/}
-      {/*  className='px-8 md:px-16 xl:px-48 py-16 relative'*/}
-      {/*  style={{ clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)' }}*/}
-      {/*>*/}
-      {/*  <Image*/}
-      {/*    src={gfx_bg_orange}*/}
-      {/*    alt=''*/}
-      {/*    className='fixed left-0 top-0 w-screen h-screen object-cover object-center -z-10 opacity-10'*/}
-      {/*  />*/}
-      {/*  <h1*/}
-      {/*    dangerouslySetInnerHTML={{*/}
-      {/*      __html: data.acf?.page_title ?? data.title?.rendered,*/}
-      {/*    }}*/}
-      {/*    className='text-3xl md:text-6xl font-normal text-center'*/}
-      {/*  />*/}
+    <div
+      className='px-8 md:px-16 xl:px-48 py-16 relative'
+      style={{ clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)' }}
+    >
+      <Image
+        src={gfx_bg_orange}
+        alt=''
+        className='fixed left-0 top-0 w-screen h-screen object-cover object-center -z-10 opacity-10'
+      />
+      {/*<h1*/}
+      {/*  dangerouslySetInnerHTML={{*/}
+      {/*    __html: data.acf?.page_title ?? data.title?.rendered,*/}
+      {/*  }}*/}
+      {/*  className='text-3xl md:text-6xl font-normal text-center'*/}
+      {/*/>*/}
 
+      <HomeWrapper lang={lang} timeLineEventsDe={timeLineEventsDe} timeLineEventsEn={timeLineEventsEn} allMedia={allMedia} baseLink={baseLink} searchParams={searchParams }/>
 
-      {/*  {data.acf?.text?.length > 0 && (*/}
-      {/*    <WysiwygContent*/}
-      {/*      content={data.acf?.text}*/}
-      {/*      className='text-lg md:text-xl mt-16 font-light'*/}
-      {/*    />*/}
-      {/*  )}*/}
-      {/*</div>*/}
-    </>
-
+      {data.acf?.text?.length > 0 && (
+        <WysiwygContent
+          content={data.acf?.text}
+          className='text-lg md:text-xl mt-16 font-light'
+        />
+      )}
+    </div>
   );
 };
 
