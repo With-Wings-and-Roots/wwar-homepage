eb72fccaa95dcca4ec602bce26ebbaadb2e2d3a8
volkram <volkram@volkramweber.de>
2024-02-19 22:45:19 +0100

    Revert "Fix home overflow"
    This reverts commit 8bbdc4763560289c1f8769c962c4b74feeffd041.

--- components/home/homeWrapper.jsx
+++ components/home/homeWrapper.jsx
@@ -4,7 +4,7 @@ import TimelinesPageWrapper from '@/components/timelines/timelinesPageWrapper';
 
 const HomeWrapper=({lang, timeLineEventsDe, timeLineEventsEn, allMedia, searchParams, baseLink})=>{
   return(
-    <div className={`w-full`}>
+    <div className={`w-screen`}>
       <HomeTitle />
       <TimelinesPageWrapper lang={lang} timeLineEventsDe={timeLineEventsDe} timeLineEventsEn={timeLineEventsEn} allMedia={allMedia} baseLink={baseLink} searchParams={searchParams } skip={true}/>
     </div>
