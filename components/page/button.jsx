28479afa1a41b3e9811a9e779bbeb30a44aa9f8b
volkram <volkram@volkramweber.de>
2024-02-20 19:46:28 +0100

    fix collaborators popup

--- components/page/button.jsx
+++ components/page/button.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const Button = ({ name, color, textDark=false }) => {
+const Button = ({ name, color, textDark = false }) => {
   return (
     <div
       className={`${
         color === 'turquoise' ? 'bg-wwr_turquoise' : 'bg-wwr_yellow_orange'
-      } ${textDark? 'text-wwr_rich_black':'text-wwr_white'} w-max py-2 px-4 text-base md:text-lg font-light`}
+      } ${
+        textDark ? 'text-wwr_rich_black' : 'text-wwr_white'
+      } w-max py-2 px-4 text-base md:text-lg font-light`}
     >
       {name}
     </div>
