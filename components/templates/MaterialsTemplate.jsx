05c06dcab8235bdbd6c92d854365d28b355784da
volkram <volkram@volkramweber.de>
2024-03-06 09:38:46 +0100

    fix materials links

--- components/templates/MaterialsTemplate.jsx
+++ components/templates/MaterialsTemplate.jsx
@@ -39,16 +39,29 @@ const MaterialsTemplate = ({ data }) => {
     } else if (btn.target[0].link) {
       return btn.target[0].link;
     } else if (btn.target[0].page) {
-      return btn.target[0].page;
+      return createLocalLink(btn.target[0].page);
     }
     return '#';
+  };
+
+  const shouldOpenInNewTab = (btn) => {
+    if (btn.target[0].file && btn.target[0].file.url) {
+      return true;
+    } else if (btn.target[0].link) {
+      return true;
+    } else if (btn.target[0].page) {
+      return false;
+    }
+    return false;
   };
 
   const renderButtons = (btns) => {
     return btns.map((btn, bI) => (
       <Link
         key={bI}
-        href={createLocalLink(getUrlFromButtonTarget(btn))}
+        href={getUrlFromButtonTarget(btn)}
+        target={shouldOpenInNewTab(btn) ? '_blank' : '_self'}
+        rel={shouldOpenInNewTab(btn) ? 'noopener noreferrer' : ''}
         className='bg-wwr_yellow_orange text-sm lg:text-lg  font-normal px-5 py-2 hover:text-white transition-all uppercase inline-flex'
       >
         {btn.label}
