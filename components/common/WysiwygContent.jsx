e3a9cebb50f0c22c23aa0aba75df2427d1ba80a5
MARSIF123 <maryamw561@gmail.com>
2025-12-03 20:51:46 -0500

    read more button

--- components/common/WysiwygContent.jsx
+++ components/common/WysiwygContent.jsx
@@ -1,10 +1,39 @@
+'use client';
+
+import { useState } from 'react';
+
 const WysiwygContent = ({ content, className = '', ...otherProps }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  if (!content) return null;
+
+  // Split content at WordPress <!--more--> tag
+  const [beforeMore, afterMore] = content.split('<!--more-->');
+
   return (
-    <div
-      dangerouslySetInnerHTML={{ __html: content || '' }}
-      {...otherProps}
-      className={`WysiwygContent ${className}`}
-    />
+    <div className={`WysiwygContent ${className}`} {...otherProps}>
+      <div dangerouslySetInnerHTML={{ __html: beforeMore }} />
+
+      {afterMore && (
+        <div>
+          {expanded && <div dangerouslySetInnerHTML={{ __html: afterMore }} />}
+          <span
+            onClick={() => setExpanded(!expanded)}
+            style={{
+              cursor: 'pointer',
+              fontWeight: 'inherit',
+              fontSize: 'inherit',
+              color: 'inherit',
+              textDecoration: 'underline',
+              display: 'inline-block',
+              marginTop: '0.25em',
+            }}
+          >
+            {expanded ? 'See Less' : 'See Full'}
+          </span>
+        </div>
+      )}
+    </div>
   );
 };
 
