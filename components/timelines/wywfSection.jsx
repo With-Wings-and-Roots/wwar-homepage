24515ea0bc79bd5a0dbb882948b9d13b5cf03a8f
MARSIF123 <maryamw561@gmail.com>
2026-07-24 11:40:33 -0400

    change the image to video on timeline overview page

--- components/timelines/wywfSection.jsx
+++ components/timelines/wywfSection.jsx
@@ -11,6 +11,7 @@ export default function WhatYouWillFindSection({
   previewAlt = 'Timeline preview',
 }) {
   const [isOpen, setIsOpen] = useState(false);
+  console.log(previewImage);
 
   return (
     <>
@@ -25,54 +26,31 @@ export default function WhatYouWillFindSection({
             <WysiwygContent className='prose max-w-none' content={content} />
           </div>
 
-          {/* RIGHT COLUMN: Clickable Image */}
+          {/* RIGHT COLUMN: Vimeo Video */}
           {previewImage && (
-            <div
-              onClick={() => setIsOpen(true)}
-              className='relative h-96 w-full border border-gray-200 overflow-hidden cursor-pointer group'
-            >
-              <Image
-                src={previewImage}
-                alt={previewAlt}
-                fill
-                className='object-cover group-hover:scale-105 transition-transform duration-300'
-              />
+            <div className='w-full'>
+              <div
+                className='
+        relative
+        overflow-hidden
+        rounded-xl
+        border border-gray-200
+        shadow-sm
+        aspect-video
 
-              {/* Tooltip Overlay */}
-              <div className='absolute inset-0 bg-black/40 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-end justify-center'>
-                <div className='mb-4 px-4 py-2 bg-black/70 text-white text-sm rounded-full'>
-                  Click to enlarge
-                </div>
-              </div>
+        [&>iframe]:absolute
+        [&>iframe]:top-0
+        [&>iframe]:left-0
+        [&>iframe]:w-full
+        [&>iframe]:h-full
+        [&>iframe]:border-0
+      '
+                dangerouslySetInnerHTML={{ __html: previewImage }}
+              />
             </div>
           )}
         </div>
       </section>
-
-      {/* FULLSCREEN MODAL */}
-      {isOpen && (
-        <div
-          className='fixed inset-0 z-[9999] bg-black/90 flex items-center justify-center p-6'
-          onClick={() => setIsOpen(false)}
-        >
-          <div className='relative w-full h-full max-w-6xl max-h-[90vh]'>
-            <Image
-              src={previewImage}
-              alt={previewAlt}
-              fill
-              className='object-contain'
-            />
-          </div>
-
-          {/* Close Button */}
-          <button
-            onClick={() => setIsOpen(false)}
-            className='absolute top-6 right-6 text-white text-3xl'
-          >
-            ✕
-          </button>
-        </div>
-      )}
     </>
   );
 }
